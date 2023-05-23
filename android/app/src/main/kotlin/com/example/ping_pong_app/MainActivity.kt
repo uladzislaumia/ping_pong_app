0d0bae3685d6b6f16a2eef436f55a5848d550b3d
Vladislav Myasnikov <vladislav.myasnikov777@gmail.com>
2023-05-23 19:14:18 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ping_pong_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ping_pong_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:ping_pong_app/app.dart';
+
+void main() {
+  runApp(const App());
+}
